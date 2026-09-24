39f17bec483dc6b02e03462a260f4a9c05217afd
rowan lynn <rowan@lynntribe.net>
2026-09-24 12:27:21 -0700

    created Mod1 java classes

---
+++ Mod1/src/DistanceSquared.java
@@ -0,0 +1,5 @@
+public class DistanceSquared {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Mod1/src/TwentyFivePerLine.java
@@ -0,0 +1,5 @@
+public class TwentyFivePerLine {
+    public static void main(String[] args) {
+
+    }
+}
