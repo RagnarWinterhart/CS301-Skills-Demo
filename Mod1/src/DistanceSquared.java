8512e452e60c82310f07bd762b0d1efc7973ae28
rowan lynn <rowan@lynntribe.net>
2026-09-24 15:54:09 -0700

    completed distancesquared

--- Mod1/src/DistanceSquared.java
+++ Mod1/src/DistanceSquared.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class DistanceSquared {
     public static void main(String[] args) {
+        /* two integer cmd line arg input
+         * prints Euclidean distance between point (x,y)
+         * and origin (0,0), but the result is squared
+        */
+        double originX = 0;
+        double originY = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a real number value for x");
+        double x = scanner.nextDouble();
+        System.out.println("Please enter a real number value for y");
+        double y = scanner.nextDouble();
+        scanner.close();
+
+        distance(x, y, originX, originY);
+    }
+
+    public static void distance(double x, double y,
+                                double originX, double originY){
+        double dx = originX - x; // calculate the deltas
+        double dy = originY - y;
+
+        double dist = Math.hypot(dx, dy); // take the hypotenuse
+        double distSq = dist * dist; // square the result
+
+        System.out.println("The Eculidean distance between point " +
+                "(" + x + ", " + y + ") and the origin is: " + dist + " \nand that " +
+                "distance squared is: " + distSq);
 
     }
 }
--- Mod1/src/TwentyFivePerLine.java
+++ Mod1/src/TwentyFivePerLine.java
@@ -1,5 +1,9 @@
 public class TwentyFivePerLine {
     public static void main(String[] args) {
-
+        /*
+        Write a program TwentyFivePerLine.java that, using one for loop
+        and one if statement, prints the integers from
+        1000 to 2000 with 25 integers per line. Hint: use the % operator.
+         */
     }
 }
