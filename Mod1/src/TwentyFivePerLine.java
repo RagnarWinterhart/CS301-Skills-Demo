55a551eee44cb4c94843717a4b540c67bef26e12
rowan lynn <rowan@lynntribe.net>
2026-09-25 10:59:21 -0700

    Completed TwentyFivePerLine class

--- Mod1/src/TwentyFivePerLine.java
+++ Mod1/src/TwentyFivePerLine.java
@@ -5,5 +5,12 @@ public class TwentyFivePerLine {
         and one if statement, prints the integers from
         1000 to 2000 with 25 integers per line. Hint: use the % operator.
          */
+        for(int i = 1000; i <= 2000; i++){
+            System.out.print(i + " "); // prints next int
+            if((i - 1000 + 1 ) % 25 == 0){
+                System.out.println(); // Starts new line once we hit 25 ints
+                                        // on previous line
+            }
+        }
     }
 }
