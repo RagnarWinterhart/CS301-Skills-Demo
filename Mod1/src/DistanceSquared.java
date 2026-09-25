e4f30cca2293d168f862cd920bfa672a5670a2d0
rowan lynn <rowan@lynntribe.net>
2026-09-25 10:39:51 -0700

    Completed Ordered class

--- Mod1/src/DistanceSquared.java
+++ Mod1/src/DistanceSquared.java
@@ -10,26 +10,39 @@ public class DistanceSquared {
         double originY = 0;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter a real number value for x");
+        System.out.println("Please enter a real number value for x: ");
+        //checking for valid, input and reprompt
+        while(!scanner.hasNextDouble()){
+            System.out.println("Invalid input. Please enter a " +
+                    "valid real number for x: ");
+            scanner.next();
+        }
         double x = scanner.nextDouble();
-        System.out.println("Please enter a real number value for y");
-        double y = scanner.nextDouble();
-        scanner.close();
 
+        System.out.println("Please enter a real number value for y: ");
+        while(!scanner.hasNextDouble()){
+            System.out.println("Invalid input. Please enter a " +
+                    "valid real number for y: ");
+            scanner.next();
+        }
+        double y = scanner.nextDouble();
+
+        scanner.close();
         distance(x, y, originX, originY);
     }
 
     public static void distance(double x, double y,
                                 double originX, double originY){
-        double dx = originX - x; // calculate the deltas
+        // calculate the delta x and y
+        double dx = originX - x;
         double dy = originY - y;
 
-        double dist = Math.hypot(dx, dy); // take the hypotenuse
-        double distSq = dist * dist; // square the result
+        double dist = Math.hypot(dx, dy); // gives us the length
+        double distSq = dist * dist; // kind of redundant, since Math.hypot takes
+                                    // the sqrt, could do distSq = (dx*dx) + (dy*dy)
 
-        System.out.println("The Eculidean distance between point " +
-                "(" + x + ", " + y + ") and the origin is: " + dist + " \nand that " +
-                "distance squared is: " + distSq);
+        System.out.println("The squared Eculidean distance between point " +
+                "(" + x + ", " + y + ") and the origin is: " + distSq);
 
     }
 }
--- Mod1/src/Ordered.java
+++ Mod1/src/Ordered.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Ordered {
     public static void main(String[] args) {
         /*
@@ -6,5 +8,44 @@ public class Ordered {
         is true if the three values are either in ascending or in descending
         order, and false otherwise. Print the variable b.
          */
+        boolean b = false;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter an integer for x: ");
+        //checking for valid input and reprompt
+        while(!scanner.hasNextInt()){
+            System.out.println("Invalid input. Please enter an " +
+                    "integer for x: ");
+            scanner.next();
+        }
+        int x = scanner.nextInt();
+
+        System.out.println("Please enter an integer for y: ");
+        while(!scanner.hasNextInt()){
+            System.out.println("Invalid input. Please enter an " +
+                    "integer for y: ");
+            scanner.next();
+        }
+        int y = scanner.nextInt();
+
+        System.out.println("Please enter an integer for z: ");
+        while(!scanner.hasNextInt()){
+            System.out.println("Invalid input. Please enter an " +
+                    "integer for z: ");
+            scanner.next();
+        }
+        int z = scanner.nextInt();
+
+        scanner.close();
+
+        isAscending(x, y, z, b);
+    }
+
+    public static void isAscending(int x, int y, int z, boolean b){
+        // only assigns b true if ascending or descending
+        if ((x > y && y > z) || (x < y && y < z)){
+            b = true;
+        }
+        System.out.println(b);
     }
 }
